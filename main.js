ee05dc6d3c8a64975dbfb0a9c34b5f0435710a0b
Nivando <sidvandoni@gmail.com>
2020-04-08 22:49:32 -0300

    added axios lib

---
+++ main.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+const ApiKey = "6764c012e0d24111a71f9c0053acbfba";
+const Argument = "top-headlines?";
+const country = "pt";
+ axios.get(`https://newsapi.org/v2/${Argument}country=${country}&apiKey=${ApiKey}`).then(resp => {
+    let obj = resp.data.articles;
+    console.log(obj[1]);
+        
+        
+});
+
+
+ 
